d2c6381b07c9613e3801eccfbd05bc78693bbf78
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2024-10-17 17:08:35 +0900

    update

---
+++ Operator/src/AssignmentOperator.java
@@ -0,0 +1,28 @@
+public class AssignmentOperator {
+    public static void main(String[] args) {
+        // 대입연산자
+        int value1 = 3;
+        value1 = value1 + 3;
+        System.out.println(value1); // 6
+        System.out.println();
+
+        int value2;
+        value2 = 5;
+        System.out.println(value2 += 2); // value2 = value2 + 2 // 7
+        value2 = 5;
+        System.out.println(value2 -= 2); // value2 = value2 - 2 // 3
+        value2 = 5;
+        System.out.println(value2 *= 2); // 10
+        value2 = 5;
+        System.out.println(value2 /= 2); // 2
+        value2 = 5;
+        System.out.println(value2 %= 2); // 1
+    }
+}
+
+
+
+
+
+
+
