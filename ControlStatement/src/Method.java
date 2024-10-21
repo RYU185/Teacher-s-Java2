77834246298501f436443a6278dc5eaac8e746d7
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2024-10-21 09:48:39 +0900

    Control

---
+++ ControlStatement/src/Method.java
@@ -0,0 +1,26 @@
+public class Method {
+    public static void main(String[] args) {
+        // 메서드없이 홀짝을 구분하는 코드
+        int num = 4;
+        String str = (num % 2 == 0) ? "짝수" : "홀수";
+        System.out.println(str);
+        num = 5;
+        str = (num % 2 == 0) ? "짝수" : "홀수";
+        System.out.println(str);
+        num = 6;
+        str = (num % 2 == 0) ? "짝수" : "홀수";
+        System.out.println(str);
+        num = 7;
+        str = (num % 2 == 0) ? "짝수" : "홀수";
+        System.out.println(str);
+        num = 8;
+        str = (num % 2 == 0) ? "짝수" : "홀수";
+        System.out.println(str);
+        num = 9;
+        str = (num % 2 == 0) ? "짝수" : "홀수";
+        System.out.println(str);
+        num = 10;
+        str = (num % 2 == 0) ? "짝수" : "홀수";
+        System.out.println(str);
+    }
+}
