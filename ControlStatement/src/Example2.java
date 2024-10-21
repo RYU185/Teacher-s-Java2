ad1ea9fb183310fb72f8128665d84bad85acf602
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2024-10-21 12:41:54 +0900

    add

--- ControlStatement/src/Example2.java
+++ ControlStatement/src/Example2.java
@@ -6,8 +6,12 @@ public class Example2 {
         System.out.println(length);
         // 문자열의 길이를 반환하는 메서드
         // (문제) 아래 코드가 작동하도록 메서드를 만드시오.
-        System.out.println(getLength("Hello World"));
-        System.out.println(getLength("Please help me"));
-        System.out.println(getLength("Happy birthday"));
+        System.out.println(getLength("Hello World")); // 11
+        System.out.println(getLength("Please help me")); // 14
+        System.out.println(getLength("Happy birthday")); // 14
+    }
+
+    public static int getLength(String str) {
+        return str.length();
     }
 }
