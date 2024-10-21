a4f8aded9cb4a3161d288f24087f82cd24edc634
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2024-10-21 12:24:07 +0900

    Update Example2.java

--- ControlStatement/src/Example2.java
+++ ControlStatement/src/Example2.java
@@ -1,2 +1,13 @@
 public class Example2 {
+    public static void main(String[] args) {
+        // 다양한 메서드 만들어보기
+        String str = "Hello";
+        int length = str.length();
+        System.out.println(length);
+        // 문자열의 길이를 반환하는 메서드
+        // (문제) 아래 코드가 작동하도록 메서드를 만드시오.
+        System.out.println(getLength("Hello World"));
+        System.out.println(getLength("Please help me"));
+        System.out.println(getLength("Happy birthday"));
+    }
 }
