a0d91e8583b59733ae61e5832dbbcb1f76dcffae
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2024-10-24 14:14:05 +0900

    Update Example13.java

--- ControlStatement/src/Example13.java
+++ ControlStatement/src/Example13.java
@@ -6,9 +6,23 @@ public class Example13 {
         // 대문자 'A'=65 ~ 'Z'=90
         // 소문자 'a'=97 ~ 'z'=122
         // 91=[, 92=\, 93=], 94=^, 95=_, 96=`
-//        System.out.println(findCharCount('A','Z')); // 26
-//        System.out.println(findCharCount('h','q')); // 10
-//        System.out.println(findCharCount('W','p')); // 20
+        System.out.println(findCharCount('A','Z')); // 26
+        System.out.println(findCharCount('h','q')); // 10
+        System.out.println(findCharCount('W','p')); // 20
     }
-
+    public static int findCharCount(char a, char b) {
+        int count = 0;
+        for (int i=(int)a; i<=(int)b; i++) {
+            if (!(i>='[' && i<='`')) { // 91~96에 속하지 않으면, (96 ` 백틱)
+                count = count + 1;
+            }
+        }
+        return count;
+    }
 }
+
+
+
+
+
+
