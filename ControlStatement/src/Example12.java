bb0a112371d946e57c8237e670e089f410f6e3f9
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2024-10-24 14:03:03 +0900

    Update Example12.java

--- ControlStatement/src/Example12.java
+++ ControlStatement/src/Example12.java
@@ -5,8 +5,32 @@ public class Example12 {
         // 첫번째 매개변수 : 숫자범위의 시작
         // 두번째 매개변수 : 숫자범위의 끝
         // 세번째 매개변수 : 찾을 숫자
-//        System.out.println(findNumber(1,10,5)); // "5"
-//        System.out.println(findNumber(-10,10,0)); // "0"
-//        System.out.println(findNumber(20,50,55)); // "없음"
+        System.out.println(findNumber(1,10,5)); // "5"
+        System.out.println(findNumber(-10,10,0)); // "0"
+        System.out.println(findNumber(20,50,55)); // "없음"
+    }
+//    public static String findNumber(int a, int b, int c) {
+//        for (int i=a; i<=b; i++) {
+//            if (i==c) {
+//                return ""+c;
+//                //return Integer.toString(c);
+//            }
+//        }
+//        return "없음";
+//    }
+
+    public static String findNumber(int a, int b, int c) {
+        boolean found = false;
+        for (int i=a; i<=b; i++) {
+            if (i==c) {
+                found = true;
+                break;
+            }
+        }
+        if (found) {
+            return ""+c;
+        }else {
+            return "없음";
+        }
     }
 }
