3935c549ca2a4708ced87a6497727b90d6f5ad4b
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2024-10-25 09:55:40 +0900

    Create Array2D.java

---
+++ ArrayString/src/Array2D.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public class Array2D {
+    public static void main(String[] args) {
+        // 2차원 배열의 선언
+        int[][] array1 = new int[3][4];
+        int[][] array2;
+        array2 = new int[3][4];
+
+        //int array3[][]; // 다른 선언방법. 잘 안씀!
+        //int[] array4[]; // 다른 선언방법. 잘 안씀!
+    }
+}
