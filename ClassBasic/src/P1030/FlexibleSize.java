3510c1e8323326beffb1efdc39fc584fd76a6024
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2024-10-30 12:06:36 +0900

    Update FlexibleSize.java

--- ClassBasic/src/P1030/FlexibleSize.java
+++ ClassBasic/src/P1030/FlexibleSize.java
@@ -2,6 +2,12 @@ package P1030;
 
 public class FlexibleSize {
     public static void main(String[] args) {
+        // #1. method1(int... value)
 
+    }
+    public static void method1(int... values){
+        for(int i=0; i<values.length; i++) {
+            System.out.println(values[i] + " ");
+        }
     }
 }
