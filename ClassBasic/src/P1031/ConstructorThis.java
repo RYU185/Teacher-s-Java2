4bc533f08588e91c8fc430977b8ece222673c300
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2024-10-31 11:39:47 +0900

    Update ConstructorThis.java

--- ClassBasic/src/P1031/ConstructorThis.java
+++ ClassBasic/src/P1031/ConstructorThis.java
@@ -2,20 +2,22 @@ package P1031;
 
 public class ConstructorThis {
     public static void main(String[] args) {
-        E1 e1 = new E1(3,5);
-        e1.print();
+        AAA a = new AAA(3,5);
+        a.print();
         // 클래스의 필드와 메서드의 지역변수가 같으면 필드를 지역변수로 인식하므로
         // 명시적으로 this를 사용하여 필드를 표시해야 함. (this.필드)
-        E2 e2 = new E2();
-        e2.print(); // 1 2 3 4
-        E2 e2_1 = new E2(10);
-        e2_1.print(); // 10 2 3 4 (m1만 10으로 초기화)
+        BBB b1 = new BBB();
+        b1.print(); // 1 2 3 4
+        BBB b2 = new BBB(10);
+        b2.print(); // 10 2 3 4 (m1만 10으로 초기화)
+        BBB b3 = new BBB(20, 30);
+        b3.print(); // 20 30 3 4
     }
 }
-class E1 {
+class AAA {
     int m;
     int n;
-    E1(int m, int n) {
+    AAA(int m, int n) {
         m = m;
         this.n = n;
     }
@@ -23,20 +25,24 @@ class E1 {
         System.out.println(m + " " + n);
     }
 }
-class E2 {
+class BBB {
     int m1;
     int m2;
     int m3;
     int m4;
-    E2() { // 기본생성자
+    BBB() { // 기본생성자
         this.m1 = 1;
         this.m2 = 2;
         this.m3 = 3;
         this.m4 = 4;
     }
-    E2(int m1) {
+    BBB(int m1) {
         this();
         this.m1 = m1;
+    }
+    BBB(int m1, int m2) {
+        this(m1);
+        this.m2 = m2;
     }
     void print() {
         System.out.println(m1 + " " + m2 + " " + m3 + " " + m4);
