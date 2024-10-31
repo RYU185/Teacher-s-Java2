b97633d56e63389699b4fce151d409c6064d95cb
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2024-10-31 10:33:44 +0900

    Update Constructor2.java

--- ClassBasic/src/P1031/Constructor2.java
+++ ClassBasic/src/P1031/Constructor2.java
@@ -3,7 +3,10 @@ package P1031;
 public class Constructor2 {
     // 생성자의 오버로딩
     public static void main(String[] args) {
-
+        AA a1 = new AA();
+        AA a2 = new AA(3);
+        AA a3 = new AA("hello");
+        AA a4 = new AA(3,4);
     }
 }
 class AA {
@@ -17,16 +20,16 @@ class AA {
     }
     AA(int a) {
         a = a;
-        System.out.println("매개변수 1개 int");
+        System.out.println("매개변수 1개 int " + a );
     }
     AA(String str) { // 매개변수의 타입이 달라도 오버로딩 성립!!
         str = str;
-        System.out.println("매개변수 1개 문자열");
+        System.out.println("매개변수 1개 문자열 " + str);
     }
     AA(int a, int b) {
         a = a;
         b = b;
-        System.out.println("매개변수 2개");
+        System.out.println("매개변수 2개 " + a + " " + b);
     }
 }
 
