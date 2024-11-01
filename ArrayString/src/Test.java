d7301a2ec973ff176365bd7c2f60ecfcd360debb
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2024-11-01 16:17:46 +0900

    Update Test.java

--- ArrayString/src/Test.java
+++ ArrayString/src/Test.java
@@ -11,8 +11,13 @@ public class Test {
 //                return arr[i];
 //            }
 //        }
+//        for (int i=0; i< arr.length; i++) {
+//            if (arr[i].indexOf(ch) == 0) {
+//                return arr[i];
+//            }
+//        }
         for (int i=0; i< arr.length; i++) {
-            if (arr[i].indexOf(ch) == 0) {
+            if (arr[i].substring(0,1).equals(ch+"")) {
                 return arr[i];
             }
         }
