a9b0164423c675d14cd03b463148f62702ff5f56
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2024-11-01 12:09:06 +0900

    Update Modifier_2.java

--- Modifier/src/Modifier_2/Modifier_2.java
+++ Modifier/src/Modifier_2/Modifier_2.java
@@ -9,7 +9,7 @@ public class Modifier_2 {
         // AA, BB, CC 클래스 인스턴스화
         AA a = new AA();
         //BB b = new BB(); // 임포트가 안되므로 인스턴스화할 수 없음
-        CC c = new CC(); // 정의한 기본생성자(자동아님)가 default이므로
+        //CC c = new CC(); // 정의한 기본생성자(자동아님)가 default이므로
                          // 외부패키지에서 호출할 수 없음. 인스턴스화 불가!
     }
 }
