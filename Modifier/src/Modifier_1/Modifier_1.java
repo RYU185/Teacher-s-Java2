ee6ea1a7426faf78a6d9c4fb055d362816a1c0d0
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2024-11-01 11:21:53 +0900

    add

--- Modifier/src/Modifier_1/Modifier_1.java
+++ Modifier/src/Modifier_1/Modifier_1.java
@@ -2,6 +2,7 @@ package Modifier_1;
 
 import Modifier_1.pack1.A;
 import Modifier_1.pack1.B;
+import Modifier_1.pack2.C;
 
 public class Modifier_1 {
     public static void main(String[] args) {
@@ -9,5 +10,7 @@ public class Modifier_1 {
         a.print(); // 1 2 3 4
         B b = new B();
         b.print(); // 1 2 3
+        C c = new C();
+        c.print(); // 1
     }
 }
