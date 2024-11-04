6191b96fb39328b953e1b7888e905d1a8ea8a2f7
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2024-11-04 11:41:12 +0900

    Update StaticBlock.java

--- Modifier/src/StaticBlock.java
+++ Modifier/src/StaticBlock.java
@@ -1,13 +1,13 @@
 public class StaticBlock {
     public static void main(String[] args) {
-
+        System.out.println(B.b); // 5
     }
 }
 
 class B {
      int a;
      static int b;
-     static {
+     static { // 스태틱 블록 (정적필드 초기화)
          b = 5;
          System.out.println("클래스 B가 로딩되었습니다.");
      }
