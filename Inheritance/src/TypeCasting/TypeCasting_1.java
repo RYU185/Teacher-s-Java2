ef9e770ccf1b7ad1433ad3375946621f119d7cc3
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2024-11-05 11:47:25 +0900

    Update TypeCasting_1.java

--- Inheritance/src/TypeCasting/TypeCasting_1.java
+++ Inheritance/src/TypeCasting/TypeCasting_1.java
@@ -26,6 +26,14 @@ public class TypeCasting_1 {
         System.out.println(value2.b); // 4
         value2.methodA(); // A 클래스
         value2.methodB(); // B 클래스
+
+        // #3. B->A 업캐스팅
+        A value3 = new B();
+        System.out.println(value3.a); // 3
+        value3.methodA(); // A 클래스
+        B value4 = (B)value3;
+        System.out.println(value4.b); // 4
+        value4.methodB(); // B 클래스
     }
 }
 
