e0468d8255a12bd045b72347c8a1c91f4975a4ec
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2024-11-05 10:27:06 +0900

    Update Polymorphism.java

--- Inheritance/src/Inheritance_1/Polymorphism.java
+++ Inheritance/src/Inheritance_1/Polymorphism.java
@@ -9,6 +9,24 @@ class D extends B {}
 // 다형성
 public class Polymorphism {
     public static void main(String[] args) {
-
+        // A
+        A a1 = new A();
+        A a2 = new B(); // 업캐스팅 B는 A이다
+        A a3 = new C(); // 업캐스팅 C는 A이다
+        A a4 = new D(); // 업캐스팅 D는 A이다
+        // B
+        // B b1 = new A(); // 에러 다운캐스팅 : A는 B이다(X) - 잘못된 명제!
+        B b2 = new B();
+        B b3 = new C();
+        B b4 = new D();
     }
 }
+
+
+
+
+
+
+
+
+
