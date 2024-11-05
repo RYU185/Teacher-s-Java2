8f958ce7eae0f18bf29882216b6f562b6b19e3ec
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2024-11-05 11:22:35 +0900

    Create TypeCasting_1.java

---
+++ Inheritance/src/TypeCasting/TypeCasting_1.java
@@ -0,0 +1,20 @@
+package TypeCasting;
+
+class A {
+    int a = 3;
+    void methodA() {
+        System.out.println("A 클래스");
+    }
+}
+class B extends A {
+    int b = 4;
+    void methodB() {
+        System.out.println("B 클래스");
+    }
+}
+
+public class TypeCasting_1 {
+    public static void main(String[] args) {
+
+    }
+}
