1454ad8a6dd414de4b10da07d095c644a44c3cad
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2024-11-05 10:44:38 +0900

    Update Polymorphism.java

--- Inheritance/src/Inheritance_1/Polymorphism.java
+++ Inheritance/src/Inheritance_1/Polymorphism.java
@@ -9,22 +9,25 @@ class D extends B {}
 // 다형성
 public class Polymorphism {
     public static void main(String[] args) {
-        // A
+        // <중요> 인스턴스의 타입은 생성자에 의해 결정된다!!
+        // 대입연산자 왼쪽의 타입은 상속관계에 의해 부모클래스일 수 있음
+
+        // #1. A
         A a1 = new A();
         A a2 = new B(); // 업캐스팅 B는 A이다
         A a3 = new C(); // 업캐스팅 C는 A이다
         A a4 = new D(); // 업캐스팅 D는 A이다
-        // B
+        // #2. B
         // B b1 = new A(); // 에러 다운캐스팅 : A는 B이다(X) - 잘못된 명제!
         B b2 = new B();
         B b3 = new C();
         B b4 = new D();
-        // C
+        // #3. C
         // C c1 = new A(); // A는 C이다(X)
         // C c2 = new B(); // B는 C이다(X)
         C c3 = new C();
         // C c4 = new D(); // D는 C이다(X)
-        // D
+        // #4. D
         // D d1 = new A(); // A는 D이다(X)
         // D d2 = new B(); // B는 D이다(X)
         // D d3 = new C(); // C는 D이다(X)
