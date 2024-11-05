b06c7a1a3e7915baf26950c94aed309bfe74ece9
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2024-11-05 09:39:09 +0900

    add

---
+++ Inheritance/src/Inheritance_1/Inheritance_1.java
@@ -0,0 +1,20 @@
+package Inheritance_1;
+
+class Human {
+    String name;
+    int age;
+    void eat(){}
+    void sleep(){}
+}
+class Student extends Human {
+    int studentID;
+    void goToSchool() {}
+}
+class Worker extends Human {
+    int workerID;
+    void goToWork() {}
+}
+
+public class Inheritance_1 {
+
+}
