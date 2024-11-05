a4f90168afd45cc5e5008248673a9ab4d2af7e0c
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2024-11-05 10:11:27 +0900

    Update Polymorphism.java

--- Inheritance/src/Inheritance_1/Polymorphism.java
+++ Inheritance/src/Inheritance_1/Polymorphism.java
@@ -1,8 +1,14 @@
 package Inheritance_1;
 
+// 단순한 상속관계
+class A {}
+class B extends A {}
+class C extends B {}
+class D extends B {}
+
 // 다형성
 public class Polymorphism {
     public static void main(String[] args) {
-        
+
     }
 }
