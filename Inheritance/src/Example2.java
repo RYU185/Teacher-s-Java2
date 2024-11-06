251d186a71de8ea562474fef7b9ad62bab7ccbf4
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2024-11-07 08:56:58 +0900

    Update Example2.java

--- Inheritance/src/Example2.java
+++ Inheritance/src/Example2.java
@@ -1,5 +1,19 @@
 import java.util.Arrays;
 
+/*
+음악앨범을 관리하는 클래스
+요구사항:
+ 앨범제목 (title) - 문자열
+ 발매연도 (releaseYear) - 정수
+ 앨범내 곡들의 제목 배열 (tracks) - 문자열 배열
+
+다음 메소드들을 구현하세요:
+생성자(기본생성자, 모든 필드를 매개변수로 가지는 생성자)
+각 필드의 getter/setter 메서드
+toString 오버라이딩 메서드
+tracks 배열에 곡의 제목을 한개 추가하는 메서드 (addTracks)
+*/
+
 public class Example2 {
     public static void main(String[] args) {
         Album album1 = new Album();
