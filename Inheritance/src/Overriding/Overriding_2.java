60790202a61ccd4088c09d2bdf2f4ce8b2e9c162
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2024-11-06 10:38:03 +0900

    add

---
+++ Inheritance/src/Overriding/Overriding_2.java
@@ -0,0 +1,17 @@
+package Overriding;
+
+class Animal {
+    void cry() {
+        System.out.println("...");
+    }
+}
+// 자식클래스 새, 고양이, 개
+class Bird extends Animal {}
+class Cat extends Animal {}
+class Dog extends Animal {}
+
+public class Overriding_2 {
+    public static void main(String[] args) {
+
+    }
+}
