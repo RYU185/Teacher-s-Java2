f3c6763acaa873974d563de4059e694cf681a0a3
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2024-11-06 11:42:08 +0900

    Update Overriding_3.java

--- Inheritance/src/Overriding/Overriding_3.java
+++ Inheritance/src/Overriding/Overriding_3.java
@@ -1,7 +1,18 @@
 package Overriding;
 
+class AA {
+    void print1() {}
+    void print2() {}
+}
+class BB extends AA {
+    @Override
+    void print1() {}
+    void print2(int a) {}
+}
+
+// 오버라이딩과 오버로딩의 비교
 public class Overriding_3 {
     public static void main(String[] args) {
-        
+
     }
 }
