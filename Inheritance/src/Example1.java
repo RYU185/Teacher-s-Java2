64d3765bb4ed8c5026b3670454499e972f131359
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2024-11-06 15:44:01 +0900

    Update Example1.java

--- Inheritance/src/Example1.java
+++ Inheritance/src/Example1.java
@@ -10,7 +10,42 @@
 각 필드의 getter/setter 메소드
 */
 class Student {
+    private String studentId;
+    private String name;
+    private int grade;
 
+    public Student() {
+    }
+
+    public Student(String studentId, String name, int grade) {
+        this.studentId = studentId;
+        this.name = name;
+        this.grade = grade;
+    }
+
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(String studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+
+    public void setGrade(int grade) {
+        this.grade = grade;
+    }
 }
 
 public class Example1 {
