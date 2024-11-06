25188c5ba811f42d11db12564fac25b8ae66e5cb
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2024-11-06 10:46:52 +0900

    Update Overriding_2.java

--- Inheritance/src/Overriding/Overriding_2.java
+++ Inheritance/src/Overriding/Overriding_2.java
@@ -24,6 +24,13 @@ class Dog extends Animal {
 
 public class Overriding_2 {
     public static void main(String[] args) {
-
+        Animal animal = new Animal();
+        Bird bird = new Bird();
+        Cat cat = new Cat();
+        Dog dog = new Dog();
+        animal.cry();
+        bird.cry();
+        cat.cry();
+        dog.cry();
     }
 }
