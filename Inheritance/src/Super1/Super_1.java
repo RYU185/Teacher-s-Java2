9870be404dfa6703e108a89a2efbdf9b80ff246d
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2024-11-07 11:15:16 +0900

    Update Super_1.java

--- Inheritance/src/Super1/Super_1.java
+++ Inheritance/src/Super1/Super_1.java
@@ -18,10 +18,10 @@ class B extends A {
 public class Super_1 {
     public static void main(String[] args) {
         B value1 = new B();
-        value1.abc();
-        value1.bcd();
+        value1.abc(); // B클래스 abc
+        value1.bcd(); // B클래스 bcd
         A value2 = (A)value1; //업캐스팅
-        value2.abc();
+        value2.abc(); // B클래스 abc (오버라이딩)
         // value2.bcd();
         // 에러.. A를 바인딩(참조)하고 있으므로 B영역에 있는
         // bcd 메서드는 호출이 불가능하다
