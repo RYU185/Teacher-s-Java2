8b2f325320ca664d9266316359149b8a426da7e5
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2024-11-07 11:06:53 +0900

    Update Super_1.java

--- Inheritance/src/Super1/Super_1.java
+++ Inheritance/src/Super1/Super_1.java
@@ -1,7 +1,21 @@
 package Super1;
 
+class A {
+    void abc() {
+        System.out.println("A클래스");
+    }
+}
+class B extends A {
+    void abc() {
+        System.out.println("B클래스 abc");
+    }
+    void bcd() {
+        System.out.println("B클래스 bcd");
+    }
+}
+
 public class Super_1 {
     public static void main(String[] args) {
-        
+
     }
 }
