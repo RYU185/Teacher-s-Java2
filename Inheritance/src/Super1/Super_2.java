0dcb1e4fa7a4fe8eecee560e5219ffdf17c1a551
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2024-11-07 11:32:40 +0900

    Update Super_2.java

--- Inheritance/src/Super1/Super_2.java
+++ Inheritance/src/Super1/Super_2.java
@@ -18,12 +18,13 @@ class CC {
 }
 class DD extends CC {
     DD() {
+        //super(); // 부모에게 기본생성자가 없으므로 에러 발생
         super(3);
     }
 }
 
 public class Super_2 {
     public static void main(String[] args) {
-
+        BB value1 = new BB();
     }
 }
