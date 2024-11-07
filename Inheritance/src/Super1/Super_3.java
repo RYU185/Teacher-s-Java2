397f6ae8828d02948c974dab2acead987a3a4305
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2024-11-07 12:36:07 +0900

    Update Super_3.java

--- Inheritance/src/Super1/Super_3.java
+++ Inheritance/src/Super1/Super_3.java
@@ -2,8 +2,9 @@ package Super1;
 
 class A1 {
     A1() {
-        this(3);
+        this(3); // this() 또는 super()는 반드시 첫번째 줄에 있어야 함!!!
         System.out.println("A1생성자 1");
+        // this(3); // 에러!!
     }
     A1(int a) {
         System.out.println("A1생성자 2");
@@ -20,9 +21,9 @@ class B1 extends A1 {
 }
 public class Super_3 {
     public static void main(String[] args) {
-        A1 value1 = new A1();
+//        A1 value1 = new A1();
 //        A1 value2 = new A1(3);
 //        B1 value3 = new B1();
-//        B1 value4 = new B1(3);
+        B1 value4 = new B1(3);
     }
 }
