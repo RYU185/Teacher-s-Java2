803d0aecbc79b8ce8c373637438bbc1ab72ccca5
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2024-11-08 10:15:02 +0900

    add

---
+++ Inheritance/src/ObjectClass/Object_2.java
@@ -0,0 +1,20 @@
+package ObjectClass;
+
+class A {
+    int a = 3;
+    int b = 4;
+}
+class B {
+    int a = 3;
+    int b = 4;
+    @Override
+    public String toString() {
+        return "필드값(a,b) = " + a + " " + b;
+    }
+}
+
+public class Object_2 {
+    public static void main(String[] args) {
+
+    }
+}
