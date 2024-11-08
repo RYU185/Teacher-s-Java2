6e0e4847116ca9c2721d95f59856dc718c685910
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2024-11-08 12:18:19 +0900

    Update Example3.java

--- Inheritance/src/Example3.java
+++ Inheritance/src/Example3.java
@@ -10,7 +10,44 @@ class Album2 {
     private String title;
     private int releaseYear;
     private List<String> tracks; // 원래 문자열의 배열이었던것을 List로 변경
-    
+
+    public Album2() {
+    }
+
+    public Album2(String title, int releaseYear, List<String> tracks) {
+        this.title = title;
+        this.releaseYear = releaseYear;
+        this.tracks = tracks;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getReleaseYear() {
+        return releaseYear;
+    }
+
+    public void setReleaseYear(int releaseYear) {
+        this.releaseYear = releaseYear;
+    }
+
+    public List<String> getTracks() {
+        return tracks;
+    }
+
+    public void setTracks(List<String> tracks) {
+        this.tracks = tracks;
+    }
+
+    @Override
+    public String toString() {
+        return "앨범제목:" + this.title + ", 발매연도:" + this.releaseYear;
+    }
 }
 
 
