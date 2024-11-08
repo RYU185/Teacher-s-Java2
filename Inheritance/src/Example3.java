e1b26c97472e7ae37c2b4328c615f46c125a1509
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2024-11-08 12:16:50 +0900

    Create Example3.java

---
+++ Inheritance/src/Example3.java
@@ -0,0 +1,21 @@
+import java.util.List;
+
+public class Example3 {
+    public static void main(String[] args) {
+
+    }
+}
+// 배열대신 컬렉션중에 하나인 ArrayList를 사용하는 예제
+class Album2 {
+    private String title;
+    private int releaseYear;
+    private List<String> tracks; // 원래 문자열의 배열이었던것을 List로 변경
+    
+}
+
+
+
+
+
+
+
