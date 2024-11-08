975c47ffc4eb5d05b8d34e7b30bda8afc021df2b
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2024-11-08 12:05:41 +0900

    Update Final_2.java

--- InterfaceAbstract/src/Modifier/Final_2.java
+++ InterfaceAbstract/src/Modifier/Final_2.java
@@ -2,16 +2,18 @@ package Modifier;
 
 class A {
     void abc() {}
-    final void bcd() {}
+    final void bcd() {} // 오버라이드 불가
 }
-class B extends A {
+class B1 extends A {
     @Override
     void abc() {
         super.abc();
     }
-    @Override
-    void bcd() {} //
+    //@Override
+    //void bcd() {} // 오버라이드 불가
 }
+final class C {}
+//class D extends C {} // final 클래스는 상속 불가!
 
 // final 메서드와 final 클래스 예제
 public class Final_2 {
