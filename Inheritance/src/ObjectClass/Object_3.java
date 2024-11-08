707797b39e41f73f994805de98f24489ba44c733
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2024-11-08 10:26:40 +0900

    Update Object_3.java

--- Inheritance/src/ObjectClass/Object_3.java
+++ Inheritance/src/ObjectClass/Object_3.java
@@ -1,6 +1,19 @@
 package ObjectClass;
 
-
+class C {
+    String name;
+    C(){}
+    C(String name){ this.name = name; }
+}
+class D {
+    String name;
+    D() {}
+    D(String name) { this.name = name; }
+    @Override
+    public boolean equals(Object obj) {
+        return this.name == ((D)obj).name ? true : false;
+    }
+}
 
 public class Object_3 {
     public static void main(String[] args) {
