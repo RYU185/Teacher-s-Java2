c673a1cc9de8fe27518800ea1555381e1fd365c8
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2024-11-08 10:06:26 +0900

    Create Object_1.java

---
+++ Inheritance/src/ObjectClass/Object_1.java
@@ -0,0 +1,10 @@
+package ObjectClass;
+
+class MyClass extends Object {}
+
+public class Object_1 {
+    public static void main(String[] args) {
+        MyClass myClass = new MyClass();
+        System.out.println(myClass.toString()); //ObjectClass.MyClass@2f4d3709
+    }
+}
