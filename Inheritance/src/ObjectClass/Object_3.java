3fb61a2bc8f53a85d88b48b90776b1b3263afd79
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2024-11-08 10:49:33 +0900

    add

--- Inheritance/src/ObjectClass/Object_3.java
+++ Inheritance/src/ObjectClass/Object_3.java
@@ -20,8 +20,12 @@ public class Object_3 {
     // Object클래스의 equals메서드와 오버라이드한 메서드의 차이 예제
         C c1 = new C("Hello");
         C c2 = new C("Hello");
-        System.out.println(c1 == c2); //
-        System.out.println(c1.equals(c2)); //
+        System.out.println(c1 == c2); // false
+        System.out.println(c1.equals(c2)); // false
+        D d1 = new D("Hello");
+        D d2 = new D("Hello");
+        System.out.println(d1 == d2); // false
+        System.out.println(d1.equals(d2)); // true
     }
 }
 
