1ff36ad38afb8a91a4d2363061dcc42c6cb3ed64
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2024-11-08 11:23:36 +0900

    Create Final_1.java

---
+++ InterfaceAbstract/src/Modifier/Final_1.java
@@ -0,0 +1,20 @@
+package Modifier;
+
+class A1 {
+    int a = 3;
+    final int b = 5;
+    A1() {}
+}
+class A2 {
+    int a;
+    final int b;
+    A2() { a=3; b=5; }
+}
+class A3 {
+    
+
+}
+class B {}
+
+public class Final_1 {
+}
