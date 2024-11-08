1762db483b2afdae29b116508c72eacfc0c67eab
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2024-11-08 11:24:57 +0900

    Update Final_1.java

--- InterfaceAbstract/src/Modifier/Final_1.java
+++ InterfaceAbstract/src/Modifier/Final_1.java
@@ -11,10 +11,18 @@ class A2 {
     A2() { a=3; b=5; }
 }
 class A3 {
-    
+    int a = 3;
+    final int b = 5;
+    A3() { a=5; }
 
 }
-class B {}
+class B {
+    void bcd() {
+        int a = 3;
+        final int b = 5;
+        a = 7;
+    }
+}
 
 public class Final_1 {
 }
