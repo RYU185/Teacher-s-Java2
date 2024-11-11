8bda68d5d5a880954b6f963bc6d8af61e6475a14
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2024-11-11 10:42:57 +0900

    add

---
+++ InterfaceAbstract/src/Abstract/Abstract_2.java
@@ -0,0 +1,13 @@
+package Abstract;
+
+class Animal {
+    void cry() {}
+}
+class Cat extends Animal {}
+class Dog extends Animal {}
+
+public class Abstract_2 {
+    public static void main(String[] args) {
+
+    }
+}
