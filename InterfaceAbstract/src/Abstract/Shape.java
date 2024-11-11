64a58f5655b3285656676352680a55694ecb3c3f
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2024-11-11 12:34:40 +0900

    Update Shape.java

--- InterfaceAbstract/src/Abstract/Shape.java
+++ InterfaceAbstract/src/Abstract/Shape.java
@@ -1,9 +1,12 @@
 package Abstract;
-
+// 도형 클래스
 abstract public class Shape {
     abstract double area(); // 도형의 면적을 구하는 추상메서드
 }
 class Circle extends Shape {
+    // 추상클래스의 자식클래스는 추상메서드를 오버라이드해야함 (강제성)
+    // 단, 자식클래스만의 필드, 메서드등은 자유롭게 정의할 수 있음!!!
+    // 추가로 자식클래스만의 추상메서드를 정의하는 것도 가능함
     double radius;
 
     Circle(double radius) {
