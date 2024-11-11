fe164a82e5f89d1be6d6a5f14c1c330b99e00c7f
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2024-11-11 11:07:29 +0900

    Update Abstract_2.java

--- InterfaceAbstract/src/Abstract/Abstract_2.java
+++ InterfaceAbstract/src/Abstract/Abstract_2.java
@@ -1,7 +1,12 @@
 package Abstract;
 
-class Animal {
-    void cry() {}
+// 내부에 추상메서드가 한개라도 포함된 클래스에는
+// 반드시 abstract 키워드를 클래스에 정의해야 한다!!
+abstract class Animal {
+    // 추상메서드는 본체가 없음(=중괄호{} 사용불가).
+    // 미완성이라는 의미로 중괄호없이 바로 세미콜론 사용
+    // 미완성이므로 cry 메서드를 반드시 오버라이드 하라는 뜻!
+    abstract void cry();
 }
 class Cat extends Animal {}
 class Dog extends Animal {}
