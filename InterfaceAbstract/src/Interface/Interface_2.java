161ebc8d76ba125223a20043befa6cdd53a4badc
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2024-11-12 11:04:05 +0900

    Create Interface_2.java

---
+++ InterfaceAbstract/src/Interface/Interface_2.java
@@ -0,0 +1,13 @@
+package Interface;
+
+// 인터페이스의 구성요소
+interface A1 {
+    // 필드는 상수만 있음
+    public static final int a = 3;
+    // 생성자 X
+    // 추상메서드
+    public abstract void abc();
+}
+
+public class Interface_2 {
+}
