d74b68d1505d4f4bdb770aa419078173def9e244
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2024-11-12 09:32:22 +0900

    Create Abstract_3.java

---
+++ InterfaceAbstract/src/Abstract/Abstract_3.java
@@ -0,0 +1,14 @@
+package Abstract;
+
+abstract class A {
+    abstract void abc();
+}
+
+// 추상클래스의 객체 생성
+// 원래 추상클래스는 그 자체로 인스턴스를 생성할 수 없음(자식클래스를 이용)
+// But, 익명이너클래스를 이용하면 가능
+public class Abstract_3 {
+    public static void main(String[] args) {
+
+    }
+}
