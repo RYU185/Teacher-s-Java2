0d5a6cce85652c32ceb09c13f90c0c52c1768a19
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2024-11-12 10:05:43 +0900

    add

--- InterfaceAbstract/src/Abstract/Abstract_3.java
+++ InterfaceAbstract/src/Abstract/Abstract_3.java
@@ -10,7 +10,7 @@ abstract class A {
 public class Abstract_3 {
     public static void main(String[] args) {
         // 원래 외부에 A클래스를 상속받아 오버라이드를 구현해야하나,
-        // 인스턴스를 한번만 사용할 목적으로 아래와 같이
+        // 인스턴스(a1)를 한번만 사용할 목적으로 아래와 같이
         // 익명이너클래스를 사용할 수 있음
         A a1 = new A() {
             @Override
---
+++ InterfaceAbstract/src/Interface/Interface_1.java
@@ -0,0 +1,7 @@
+package Interface;
+
+public class Interface_1 {
+    public static void main(String[] args) {
+        
+    }
+}
