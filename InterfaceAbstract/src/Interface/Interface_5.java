4b96090ad93d415555e3418c2b9754271005ba05
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2024-11-13 12:26:45 +0900

    Update Interface_5.java

--- InterfaceAbstract/src/Interface/Interface_5.java
+++ InterfaceAbstract/src/Interface/Interface_5.java
@@ -6,7 +6,11 @@ interface D1 {
         System.out.println("인터페이스 D1의 디폴트 메서드");
     }
 }
-class D2 implements D1 {}
+class D2 implements D1 {
+    @Override
+    public void abc() {} // 추상메서드 abc()의 오버라이드
+    // <중요> 디폴트메서드 bcd()는 오버라이드 안해도 됨!!
+}
 class D3 implements D1 {}
 
 public class Interface_5 {
