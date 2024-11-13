b461f8623a11ac2fa01f7afd28451f05087da8dd
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2024-11-13 09:34:02 +0900

    add

--- InterfaceAbstract/src/Example1.java
+++ InterfaceAbstract/src/Example1.java
@@ -8,10 +8,24 @@ abstract class Device {
         this.brand = brand;
         this.model = model;
     }
-    public void powerOn() {}
-    public void powerOff() {}
+    public void powerOn() {
+        System.out.println(brand + " " + model + " 전원 켜짐");
+    }
+    public void powerOff() {
+        System.out.println(brand + " " + model + " 전원 꺼짐");
+    }
     public abstract void doFunction(); // 추상메서드
-} 
+}
+// 휴대폰
+class Phone extends Device {
+    public Phone(String brand, String model) {
+        super(brand, model);
+    }
+    @Override
+    public void doFunction() {
+        System.out.println("휴대폰이 전화를 겁니다");
+    }
+}
 
 public class Example1 {
 }
