62ae5bc80edefbf6e9caaefaa7a57267d60366d9
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2024-11-14 12:13:13 +0900

    Update InnerInterface.java

--- InnerClass/src/InnerInterface.java
+++ InnerClass/src/InnerInterface.java
@@ -14,6 +14,16 @@ class C2 implements C1.D {
 
 public class InnerInterface {
     public static void main(String[] args) {
-
+        // 이너인터페이스의 객체 생성 #1. 구현클래스 이용
+        C1.D value1 = new C2();
+        value1.method1();
+        // 이너인터페이스의 객체 생성 #2. 익명이너클래스
+        C1.D value2 = new C1.D() {
+            @Override
+            public void method1() {
+                System.out.println("익명이너클래스로 객체 생성");
+            }
+        };
+        value2.method1();
     }
 }
