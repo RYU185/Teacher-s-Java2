edf203b52e8eb02d9e742c0f82375ee10d31d5e6
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2024-11-14 11:02:23 +0900

    Create InnerClass_4.java

---
+++ InnerClass/src/InnerClass_4.java
@@ -0,0 +1,18 @@
+class A4 {
+    int a = 3;
+    void abc() {
+        int b = 5;
+        class B4 {
+            void bcd() {
+                System.out.println(a);
+                System.out.println(b);
+            }
+        }
+    }
+}
+
+public class InnerClass_4 {
+    public static void main(String[] args) {
+
+    }
+}
