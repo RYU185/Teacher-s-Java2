fee0d65f3000311c9d951e6beef92ef577c3ff4e
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2024-11-14 11:48:42 +0900

    Update InnerClass_5.java

--- InnerClass/src/InnerClass_5.java
+++ InnerClass/src/InnerClass_5.java
@@ -20,15 +20,19 @@ public class InnerClass_5 {
         // #1
         A a = new B(); // 구현클래스의 B의 생성자로 객체생성 -> A로 업캐스팅
         c.method(a);
-        // #2
+        // #2 : 방법1의 단순버전. 변수를 따로 사용하지 않음
         c.method(new B());
-        // #3
+        // #3 //구현클래스의 인스턴스를 만들지않고 인터페이스A의 구현체를 만드는 방법
+        // 익명이너클래스!!!
         A aa = new A() {
             @Override
             public void abc() { System.out.println("#3방법"); }
         };
         c.method(aa);
-        // #4
-        
+        // #4 : 방법3의 단순버전. 변수를 따로 사용하지 않음
+        c.method(new A() {
+            @Override
+            public void abc() { System.out.println("#4방법"); }
+        });
     }
 }
