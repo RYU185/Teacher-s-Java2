346580b4ce6aae6a1f2a550907701290db8cce8a
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2024-11-14 11:38:36 +0900

    Update InnerClass_5.java

--- InnerClass/src/InnerClass_5.java
+++ InnerClass/src/InnerClass_5.java
@@ -15,6 +15,20 @@ class C {
 
 public class InnerClass_5 {
     public static void main(String[] args) {
-
+        C c = new C();
+        // C의 method()를 호출할때 매개변수로 A의 구현체를 전달하는 4가지 방법
+        // #1
+        A a = new B(); // 구현클래스의 B의 생성자로 객체생성 -> A로 업캐스팅
+        c.method(a);
+        // #2
+        c.method(new B());
+        // #3
+        A aa = new A() {
+            @Override
+            public void abc() { System.out.println("#3방법"); }
+        };
+        c.method(aa);
+        // #4
+        
     }
 }
