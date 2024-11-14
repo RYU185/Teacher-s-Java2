40b1e7a7b4f363f56b2a4254878717f4ffbd33be
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2024-11-14 10:28:17 +0900

    Update InnerClass_3.java

--- InnerClass/src/InnerClass_3.java
+++ InnerClass/src/InnerClass_3.java
@@ -8,14 +8,21 @@ class A3 {
         System.out.println("정적 메서드");
     }
     static class B3 {
-        void bcd() {
-
+        static void bcd() {
+            // 인스턴스 필드 a와 인스턴스 메서드 method1()은
+            // A3클래스가 메모리에 생성되어야 만들어지는 멤버이므로
+            // A3클래스의 인스턴스없이 참조가 불가능함
+            // System.out.println(a);
+            // 정적메서드는 오직 정적 필드와 메서드만 참조할 수 있음!!!
+            System.out.println(b);
+            // method1();
+            method2();
         }
     }
 }
 
 public class InnerClass_3 {
     public static void main(String[] args) {
-
+        A3.B3.bcd();
     }
 }
