2759d8a29c9eb39e584a67951dc66818ce74b663
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2024-11-14 10:18:53 +0900

    Create InnerClass_3.java

---
+++ InnerClass/src/InnerClass_3.java
@@ -0,0 +1,21 @@
+class A3 {
+    int a = 3;
+    static int b = 4;
+    void method1() {
+        System.out.println("인스턴스 메서드");
+    }
+    static void method2() {
+        System.out.println("정적 메서드");
+    }
+    static class B3 {
+        void bcd() {
+
+        }
+    }
+}
+
+public class InnerClass_3 {
+    public static void main(String[] args) {
+
+    }
+}
