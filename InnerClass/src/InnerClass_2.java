d4503ab1ff0a8016911c2d12318f93f25d6e0408
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2024-11-14 10:03:12 +0900

    Create InnerClass_2.java

---
+++ InnerClass/src/InnerClass_2.java
@@ -0,0 +1,20 @@
+class A2 {
+    int a = 3;
+    int b = 4;
+    void abc() {
+        System.out.println("A클래스 메서드");
+    }
+    class B2 {
+        int a = 5;
+        int b = 6;
+        void abc() {
+            System.out.println("B클래스 메서드");
+        }
+    }
+}
+
+public class InnerClass_2 {
+    public static void main(String[] args) {
+
+    }
+}
