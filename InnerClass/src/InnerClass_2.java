d565fae99decf9cce990f64cba18b863571ef24f
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2024-11-14 10:10:59 +0900

    Update InnerClass_2.java

--- InnerClass/src/InnerClass_2.java
+++ InnerClass/src/InnerClass_2.java
@@ -10,11 +10,17 @@ class A2 {
         void abc() {
             System.out.println("B클래스 메서드");
         }
+        void bcd() {
+            System.out.println(a);
+            abc();
+        }
     }
 }
 
 public class InnerClass_2 {
     public static void main(String[] args) {
-
+        A2 a = new A2();
+        A2.B2 b = a.new B2();
+        b.bcd();
     }
 }
