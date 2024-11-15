acf8e5f81c1d8d3937399a264d99656ae077ee6d
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2024-11-15 12:17:30 +0900

    Create Generic_4.java

---
+++ Generic/src/GenericClass/Generic_4.java
@@ -0,0 +1,17 @@
+package GenericClass;
+
+class GoodsContainer<T> {
+    private T t;
+    public T getT() {
+        return t;
+    }
+    public void setT(T t) {
+        this.t = t;
+    }
+}
+
+public class Generic_4 {
+    public static void main(String[] args) {
+
+    }
+}
