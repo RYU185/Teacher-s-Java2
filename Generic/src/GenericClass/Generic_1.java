79be4ca7aa767d6ec47b175c32982d5b6562616a
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2024-11-15 10:12:37 +0900

    add

---
+++ Generic/src/GenericClass/Generic_1.java
@@ -0,0 +1,15 @@
+package GenericClass;
+
+class Apple {}
+class Goods1 {
+    private Apple apple = new Apple();
+    public Apple getApple() {
+        return apple;
+    }
+    public void setApple(Apple apple) {
+        this.apple = apple;
+    }
+}
+
+public class Generic_1 {
+}
