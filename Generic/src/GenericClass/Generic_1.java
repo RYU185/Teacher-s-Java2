9752516828529fcc10d217769df721a2058454e8
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2024-11-15 10:15:11 +0900

    Update Generic_1.java

--- Generic/src/GenericClass/Generic_1.java
+++ Generic/src/GenericClass/Generic_1.java
@@ -10,6 +10,16 @@ class Goods1 {
         this.apple = apple;
     }
 }
+class Pencil {}
+class Goods2 {
+    private Pencil pencil = new Pencil();
+    public Pencil getPencil() {
+        return pencil;
+    }
+    public void setPencil(Pencil pencil) {
+        this.pencil = pencil;
+    }
+}
 
 public class Generic_1 {
 }
