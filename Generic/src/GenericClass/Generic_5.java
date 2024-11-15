f6f95171bd0ff71071e9f283bf7a139a2339ce79
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2024-11-15 12:48:00 +0900

    Update Generic_5.java

--- Generic/src/GenericClass/Generic_5.java
+++ Generic/src/GenericClass/Generic_5.java
@@ -21,11 +21,13 @@ class KeyValue<K,V> {
 public class Generic_5 {
     public static void main(String[] args) {
         // <문자열, 정수>
+        // 제네릭타입으로 원시자료형은 사용할 수 없음
+        // 원시자료형은 해당 wrapping class를 사용해야함 (Integer, Double등등)
         KeyValue<String, Integer> kv1 = new KeyValue<>();
         kv1.setKey("사과");
         kv1.setValue(1000);
         String key1 = kv1.getKey();
-        int value1 = kv1.getValue();
+        int value1 = kv1.getValue(); // 자바가 자동으로 형변환해줌
         // <정수, 문자열>
         KeyValue<Integer, String> kv2 = new KeyValue<>();
         kv2.setKey(404); // 웹브라우저 에러코드 404
