7de73dde8745c19ce41fb1295da7b9687704ba43
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2024-11-15 10:33:58 +0900

    Update Generic_2.java

--- Generic/src/GenericClass/Generic_2.java
+++ Generic/src/GenericClass/Generic_2.java
@@ -13,6 +13,9 @@ class Goods {
 // 모든 클래스의 부모인 Object를 이용하면 문제를 해결할 수 있음
 public class Generic_2 {
     public static void main(String[] args) {
-
+        Goods goods1 = new Goods();
+        goods1.setObject(new Apple());
+        // getObject()는 Object를 리턴하므로 Apple로 다운캐스팅 필요
+        Apple apple = (Apple)goods1.getObject(); 
     }
 }
