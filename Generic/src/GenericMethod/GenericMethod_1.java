ed9e47fa7137f6f95ce17398cdb7d8b116e129ce
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2024-11-18 09:37:40 +0900

    Update GenericMethod_1.java

--- Generic/src/GenericMethod/GenericMethod_1.java
+++ Generic/src/GenericMethod/GenericMethod_1.java
@@ -1,7 +1,20 @@
 package GenericMethod;
 
+// 일반클래스안에도 제네릭 메서드 사용 가능!
+class MyClass {
+    public <T> T method1(T t) {
+        return t;
+    }
+    public <T> boolean method2(T t1, T t2) {
+        return t1.equals(t2);
+    }
+    public <K,V> void method3(K k, V v) {
+        System.out.println(k + ":" + v);
+    }
+}
+
 public class GenericMethod_1 {
     public static void main(String[] args) {
-        
+
     }
 }
