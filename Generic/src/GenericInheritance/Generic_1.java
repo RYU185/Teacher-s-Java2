85d83fef1c74808da1ec003010d06706639d3199
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2024-11-18 14:03:10 +0900

    Update Generic_1.java

--- Generic/src/GenericInheritance/Generic_1.java
+++ Generic/src/GenericInheritance/Generic_1.java
@@ -1,7 +1,7 @@
 package GenericInheritance;
 
 class Parent<T> {
-    private T t;
+    T t;
     public T getT() {
         return t;
     }
@@ -9,7 +9,10 @@ class Parent<T> {
         this.t = t;
     }
 }
+// 제네릭클래스를 상속한 자식클래스도 제네릭클래스임!!
+// 부모의 제네릭타입 변수를 그대로 물려받음
 class Child<T> extends Parent<T> {}
+
 
 public class Generic_1 {
 }
