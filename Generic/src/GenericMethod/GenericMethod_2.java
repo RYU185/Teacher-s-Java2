204eb60f9d096ab2c618e375f1f63ad2070d6f53
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2024-11-18 10:22:29 +0900

    Update GenericMethod_2.java

--- Generic/src/GenericMethod/GenericMethod_2.java
+++ Generic/src/GenericMethod/GenericMethod_2.java
@@ -2,7 +2,9 @@ package GenericMethod;
 
 class A {
     public <T> void method(T t) {
+        // T는 어떤 클래스가 올지 모르므로 특정클래스의 내부메서드 사용불가!
         //System.out.println(t.length()); // 오류 (String의 내부메서드)
+        
         // "T는 Object클래스의 자식이다"라는 명제는 참
         // 그러므로 T에 Object의 내부메서드는 사용할 수 있음
         // T는 Object의 자식클래스이기때문
