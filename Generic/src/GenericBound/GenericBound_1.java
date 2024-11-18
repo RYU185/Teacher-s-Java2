09a0a9e82bfa221012a5953f4ed4497618a7d256
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2024-11-18 10:35:24 +0900

    Update GenericBound_1.java

--- Generic/src/GenericBound/GenericBound_1.java
+++ Generic/src/GenericBound/GenericBound_1.java
@@ -3,7 +3,8 @@ package GenericBound;
 class A {}
 class B extends A {}
 class C extends B {}
-
+// 제네릭타입 T의 범위를 제한하는 방법
+// T는 B를 상속하기때문에 T 자리에 B와 B의 자식클래스(C)만 올 수 있음!!
 class D <T extends B> {
     private T t;
     public T getT() {
