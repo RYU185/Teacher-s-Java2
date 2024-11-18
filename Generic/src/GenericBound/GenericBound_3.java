7eff56185c03b969d9f5879129ec41254c3d039b
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2024-11-18 12:43:36 +0900

    Update GenericBound_3.java

--- Generic/src/GenericBound/GenericBound_3.java
+++ Generic/src/GenericBound/GenericBound_3.java
@@ -24,13 +24,13 @@ class Test {
 public class GenericBound_3 {
     public static void main(String[] args) {
         Test t = new Test();
-        // #1 method1
+        // #1. method1
         t.method1(new Goods<A3>()); // Goods(A3)타입만 가능
         //t.method1(new Goods<B3>()); // 오류
         //t.method1(new Goods<C3>()); // 오류
         //t.method1(new Goods<D3>()); // 오류
 
-        // #2 method2
+        // #2. method2
         // 와일드카드 ?는 어떤 타입이던 그 자리에 올 수 있다는 뜻이므로
         // 모든 객체타입을 쓸 수 있음
         t.method2(new Goods<A3>());
@@ -45,6 +45,14 @@ public class GenericBound_3 {
         t.method3(new Goods<B3>());
         t.method3(new Goods<C3>());
         t.method3(new Goods<D3>());
+
+        // #4. method4
+        // 하한제한 super
+        // B3와 B3의 부모클래스만 사용 가능
+        t.method4(new Goods<A3>());
+        t.method4(new Goods<B3>());
+        //t.method4(new Goods<C3>()); // 오류
+        //t.method4(new Goods<D3>()); // 오류
     }
 }
 
