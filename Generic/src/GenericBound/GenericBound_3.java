48e441840ab03f5bcf7097b49f468226991f1a30
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2024-11-18 11:43:29 +0900

    Update GenericBound_3.java

--- Generic/src/GenericBound/GenericBound_3.java
+++ Generic/src/GenericBound/GenericBound_3.java
@@ -4,6 +4,7 @@ class A3 {}
 class B3 extends A3 {}
 class C3 extends B3 {}
 class D3 extends C3 {}
+
 class Goods<T> {
     private T t;
     public T getT() {
@@ -12,6 +13,12 @@ class Goods<T> {
     public void setT(T t) {
         this.t = t;
     }
+}
+class Test {
+    void method1(Goods<A> g) {}
+    void method2(Goods<?> g) {}
+    void method3(Goods<? extends B> g) {}
+    void method4(Goods<? super B> g) {}
 }
 
 public class GenericBound_3 {
