22e8485b62b3c732e8af79bc80f93bbc8b595542
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2024-11-19 11:35:15 +0900

    Update List_3.java

--- Collection/src/List/List_3.java
+++ Collection/src/List/List_3.java
@@ -7,11 +7,14 @@ import java.util.List;
 public class List_3 {
     public static void main(String[] args) {
         List<Integer> aList1 = new ArrayList<>();
-        // #1. add (요소추가)
+        // #1. add (요소추가, 맨뒤에 추가)
         aList1.add(3);
         aList1.add(4);
         aList1.add(5);
         System.out.println(aList1); // [3,4,5]
+        // #2. add(int index, E element) (요소추가, 특정 위치에 추가)
+        aList1.add(1,6);
+        System.out.println(aList1); // [3,6,4,5]
     }
 }
 
