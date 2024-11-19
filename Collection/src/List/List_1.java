7886905173923fb1f886eaec162167f9852d0287
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2024-11-19 10:17:35 +0900

    Update List_1.java

--- Collection/src/List/List_1.java
+++ Collection/src/List/List_1.java
@@ -1,4 +1,14 @@
 package List;
 
+import java.util.Arrays;
+
+// 배열과 리스트의 비교
 public class List_1 {
+    public static void main(String[] args) {
+        String[] array = new String[] {"A","B","C","D"};
+        System.out.println(array.length); // 4
+        array[2] = null;
+        System.out.println(array.length);
+        System.out.println(Arrays.toString(array));
+    }
 }
