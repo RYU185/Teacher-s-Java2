e44c2896f66e23c8d46a6106aa608eb5d5679f03
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2024-11-19 11:33:22 +0900

    Update List_3.java

--- Collection/src/List/List_3.java
+++ Collection/src/List/List_3.java
@@ -1,8 +1,24 @@
 package List;
 
+import java.util.ArrayList;
+import java.util.List;
+
 // ArrayList의 사용법 (메서드사용법)
 public class List_3 {
     public static void main(String[] args) {
-        
+        List<Integer> aList1 = new ArrayList<>();
+        // #1. add (요소추가)
+        aList1.add(3);
+        aList1.add(4);
+        aList1.add(5);
+        System.out.println(aList1); // [3,4,5]
     }
 }
+
+
+
+
+
+
+
+
