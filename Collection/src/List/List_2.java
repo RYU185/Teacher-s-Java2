fc2cb23fc9e35ffa5c654de3124d44c106fbff1a
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2024-11-19 11:06:21 +0900

    Update List_2.java

--- Collection/src/List/List_2.java
+++ Collection/src/List/List_2.java
@@ -1,7 +1,16 @@
 package List;
 
+import java.util.ArrayList;
+import java.util.List;
+
+// List 구현클래스의 생성 예제
 public class List_2 {
     public static void main(String[] args) {
-
+        // #1. 구현클래스를 이용하는 방법
+        List<Integer> aList1 = new ArrayList<>();
+        List<Integer> aList2 = new ArrayList<>(30);
+        // ArrayList 내부적으로 메모리용량을 미리 예약해두는 개념이며
+        // 실제 size는 아님!!
+        System.out.println(aList2.size()); // 0
     }
 }
