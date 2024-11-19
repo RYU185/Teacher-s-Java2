0fc74667dac5c9c6d896094996b3abf23866f567
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2024-11-19 12:02:14 +0900

    Update List_3.java

--- Collection/src/List/List_3.java
+++ Collection/src/List/List_3.java
@@ -21,6 +21,16 @@ public class List_3 {
         aList2.add(2);
         aList2.addAll(aList1);
         System.out.println(aList2); // [1, 2, 3, 6, 4, 5]
+        // #4. addAll (특정위치에 리스트 추가)
+        List<Integer> aList3 = new ArrayList<>();
+        aList3.add(1);
+        aList3.add(2); // [1,2]
+        aList3.addAll(1,aList3);
+        System.out.println(aList3); // [1,1,2,2]
+        // #5. set (수정)
+        aList3.set(1,5);
+        aList3.set(3,6);
+        System.out.println(aList3); // [1, 5, 2, 6]
     }
 }
 
