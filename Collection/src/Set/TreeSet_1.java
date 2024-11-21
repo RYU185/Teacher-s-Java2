5b5d75fdca8fe0b8a6977846d9747179167a5be2
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2024-11-21 09:37:23 +0900

    Update TreeSet_1.java

--- Collection/src/Set/TreeSet_1.java
+++ Collection/src/Set/TreeSet_1.java
@@ -11,7 +11,18 @@ public class TreeSet_1 {
         }
         // 50,48,46... 순서로 데이터입력했지만 자동으로 정렬되어 출력됨
         System.out.println(treeSet); // [2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40, 42, 44, 46, 48, 50]
-        
+
+        // #1. first
+        System.out.println(treeSet.first()); //
+        // #2. last
+        System.out.println(treeSet.last()); //
+        // #3. lower
+        System.out.println(treeSet.lower(26)); //
+        // #4. higher
+        System.out.println(treeSet.higher(26)); //
+        // #5. floor
+        System.out.println(treeSet.floor(25)); //
+        System.out.println(treeSet.floor(26)); //
     }
 }
 
