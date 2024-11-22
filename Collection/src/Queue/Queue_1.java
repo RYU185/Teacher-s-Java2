3bde5c2e2c82346bc1a33fab0c59df8c148232a3
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2024-11-22 10:18:10 +0900

    add

---
+++ Collection/src/Queue/Queue_1.java
@@ -0,0 +1,25 @@
+package Queue;
+
+import java.util.Queue;
+
+// 큐 (Queue) 인터페이스
+// 선입선출 FIFO (=First In First Out)
+// 주로 키입력등의 이벤트처리에 사용됨
+// 먼저 들어온 객체정보를 먼저 사용함
+// 구현체로는 LinkedList 또는 PriorityQueue를 많이 사용
+// LinkedList의 노드연결방식이 큐의 특징인 잦은 삽입과 삭제의 처리에 적합하기 때문!!
+public class Queue_1 {
+    public static void main(String[] args) {
+        // 예외처리없는 메서드 (안전장치가 없으므로 조심해서 사용)
+        Queue<Integer> q1 =
+    }
+}
+
+
+
+
+
+
+
+
+
