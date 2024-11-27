edbd9c805e5c1dcf0ed8e2f96bb5e959ca80f91e
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2024-11-27 12:13:02 +0900

    add

--- 문서/SQL/함수2_숫자함수.sql
+++ 문서/SQL/함수2_숫자함수.sql
@@ -15,6 +15,34 @@ select ceiling(123.06),
 	truncate(123.56, 1), -- 소수점이하 한자리
 	truncate(123.567, 2); -- 소수점이하 두자리
 	
+-- abs 절대값
+-- sign 음수는 -1, 양수는 1을 반환
+select abs(-120), sign(-120), sign(120);
+
+-- power 제곱
+-- sqrt(square root) 제곱근
+-- rand 랜덤넘버
+-- rand함수의 매개변수 seed. 동일 seed는 동일 랜덤넘버가 나옴
+select power(2,3); -- 8 (2^3)
+select sqrt(16); -- 4 (4*4=16)
+select rand(); -- 0과 1사이에 랜넘넘버
+select rand(100); -- 항상 동일한 랜덤넘버 
+select rand(200);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 
 
