8359a687c6436343cb849bc63d3eae3ec525ab78
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2024-11-27 13:56:51 +0900

    Update 함수3_날짜함수.sql

--- 문서/SQL/함수3_날짜함수.sql
+++ 문서/SQL/함수3_날짜함수.sql
@@ -28,15 +28,42 @@ select timestampdiff(day, now(), '2025-12-25'); -- 392
 	
 -- adddate 특정날짜에 기간을 더한 날짜
 -- subdate 특정날짜에 기간을 뺀 날짜
+-- interval 키워드를 함께 사용하면 더하거나 빼는 기간을 정할 수 있음
 select adddate(now(), 50), subdate(now(), 50);	
 select adddate(now(), -50); -- subdate와 동일한 결과	
+select adddate(now(), interval 50 year); 
+select adddate(now(), interval 50 month); 
+select adddate(now(), interval 50 day); 
+select adddate(now(), interval 50 hour); 
+
+-- last_day 그 달의 마지막 날
+-- dayofyear 현재 년도에서 며칠이 지났는지 반환
+select last_day(now());
+select last_day('2025-02-01'); -- 2025-02-28
+select dayofyear(now()); -- 332일째
+select dayofmonth(now());
+
+-- monthname 월을 영문으로 반환
+select monthname(now()); -- November 
+	
+-- weekday 요일 반환 : mysql의 경우, 월요일 0 ~ 일요일 6
+select weekday(now()); -- 2 (수요일)
+
 	
 	
 	
 	
-	
-	
-	
+
+
+
+
+
+
+
+
+
+
+
 	
 	
 	
