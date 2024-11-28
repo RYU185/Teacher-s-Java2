2058d6d610b5bcbb4eb8b435176da8f968452cac
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2024-11-28 12:29:47 +0900

    Update 함수5_집계함수.sql

--- 문서/SQL/함수5_집계함수.sql
+++ 문서/SQL/함수5_집계함수.sql
@@ -35,8 +35,24 @@ group by 도시
 having 고객수 >= 4; -- MySQL만 가능
 -- having count(*) >= 4; -- 표준 SQL에서는 이렇게 사용해야 함!! **
 
+select 도시, 고객번호, sum(마일리지) as 총마일리지
+from 고객
+where 고객번호 like 'T%'
+group by 도시, 고객번호
+having 총마일리지 >= 1000;
 
+-- 제품별 주문수량합계 구하기
+select 제품번호, sum(주문수량) as 주문수량합계
+from 주문세부
+group by 제품번호
+order by 제품번호;
 
+-- 각 제품별 주문수량이 1000개 이상인 제품만 출력 (총주문수량이 많은 순서대로 정렬)
+select 제품번호, sum(주문수량) as 총주문수량
+from 주문세부
+group by 제품번호
+having 총주문수량 >= 1000
+order by 총주문수량 desc;
 
 
 
