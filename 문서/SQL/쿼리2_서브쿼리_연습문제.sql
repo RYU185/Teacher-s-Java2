2f64aaaf034fbbf6a34ddeabf8003266f22e3424
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2024-12-04 11:20:42 +0900

    add

--- 문서/SQL/쿼리2_서브쿼리_연습문제.sql
+++ 문서/SQL/쿼리2_서브쿼리_연습문제.sql
@@ -41,16 +41,95 @@ from 제품;
 
 
 -- 7. 제품테이블에 있는 제품 중 단가(주문세부 테이블)가 가장 높은 제품명
+select 제품명, (select max(단가) from 주문세부) as 최고단가
+from 제품
+where 제품.제품번호 in
+	(select 제품번호 from 주문세부 where 단가 = 
+		(select max(단가) from 주문세부));
+	
+select 제품명, 최고단가
+from 제품
+join (
+	select 제품번호, max(단가) as 최고단가 from 주문세부 group by 제품번호
+) as 주문세부정보
+on 제품.제품번호 = 주문세부정보.제품번호
+order by 최고단가 desc 
+limit 1;
+
+select 제품명, max(주문세부.단가) as 최고단가
+from 제품 join 주문세부
+on 제품.제품번호 = 주문세부.제품번호
+group by 제품명
+order by 최고단가 desc 
+limit 1;
 
 
 -- 8. 제품테이블에 있는 제품단가가 가장 높은 제품의 주문수량합
+select 제품.제품번호, 제품명, 주문수량합
+from 제품 
+join (
+	select 제품번호, sum(주문수량) as 주문수량합 from 주문세부 group by 제품번호
+) as 주문수량정보
+on 제품.제품번호 = 주문수량정보.제품번호
+where 제품.단가 = (select max(단가) from 제품);
+
+select 제품번호, sum(주문수량) as 주문수량합
+from 주문세부
+where 제품번호 = (select 제품번호 from 제품 
+				where 단가 = (select max(단가) from 제품))
+group by 제품번호;
+
+select 제품번호, 제품명, 
+	(select sum(주문수량) from 주문세부 where 주문세부.제품번호 = 제품.제품번호) as 주문수량합 
+from 제품
+where 제품.단가 = (select max(단가) from 제품);
+
+with 최고단가정보 as (
+    select max(단가) as 최고단가
+    from 제품
+)
+select 제품.제품번호, 제품명, sum(주문수량) as 주문수량합
+from 제품
+join 주문세부 on 주문세부.제품번호 = 제품.제품번호
+join 최고단가정보 ON 최고단가정보.최고단가 = 제품.단가
+group by 제품.제품번호, 제품명;
 
 
 -- 9. '아이스크림'제품의 주문수량합
+select sum(주문수량) 주문수량합
+from 주문세부
+where 제품번호 in (select 제품번호 from 제품
+				where 제품명 like '%아이스크림%');
+			
+select 제품번호, sum(주문수량) 주문수량합
+from 주문세부
+where 제품번호 in (select 제품번호 from 제품
+				where 제품명 like '%아이스크림%')
+group by 제품번호
+order by 제품번호;
+
+select 주문세부.제품번호, sum(주문수량) as 주문수량합
+from 주문세부
+join 제품 on 주문세부.제품번호 = 제품.제품번호
+where 제품명 like '%아이스크림%'
+group by 주문세부.제품번호
+order by 주문세부.제품번호;
 
 
 -- 10. '서울특별시'고객들에 대한 주문년도별 주문건수
+select year(주문일) as 주문년도,
+	count(*) as 주문건수
+from 주문
+where 고객번호 in (select 고객번호 from 고객 where 도시 = '서울특별시')
+group by 주문년도
+order by 주문년도;	
 
+select year(주문일) as 주문년도, count(*) as 주문건수
+from 주문
+join 고객 on 주문.고객번호 = 고객.고객번호
+where 고객.도시 = '서울특별시'
+group by 주문년도
+order by 주문년도;
 
 -- ** 위 쿼리들을 모두 일반 조인쿼리로도 바꿔보세요
 
