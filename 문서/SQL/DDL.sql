c5f7159dd7732fd92808236a1309aab99ed03a93
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2024-12-04 12:36:55 +0900

    Create DDL.sql

---
+++ 문서/SQL/DDL.sql
@@ -0,0 +1,53 @@
+-- DDL.sql
+
+use testdb;
+
+/* 데이터베이스에서 사용되는 데이터타입
+1. 문자형 데이터타입 (단위는 바이트)
+char 고정길이 255
+varchar 가변길이 65535
+tinytext 255
+text 65535
+mediumtext 16,777,215
+longtext 4,294,967,295
+json 1기가
+
+2. 숫자형 데이터타입 (단위는 바이트)
+tinyint 1
+smallint 2
+mediumint 3
+int 4
+bigint 8
+float 4
+double 8
+
+3. 날짜시간형 데이터타입
+date YYYY-MM-DD
+time HH:MI:SS
+datetime YYYY-MM-DD HH:MI:SS
+timestamp YYYY-MM-DD HH:MI:SS
+
+4. 이진형(바이너리) 데이터타입 - 이미지, 오디오, 비디오 등등 (단위는 바이트)
+(blob은 binary large object의 약자임)
+tinyblob 255
+blob 65535 
+mediumblob 16,777,215
+longblob 4,294,967,295 (4기가)
+*/ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
