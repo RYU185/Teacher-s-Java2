3d4814a300598f6dc8ac42c31b7b480814976cfc
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2024-12-05 11:45:16 +0900

    Update 제약조건.sql

--- 문서/SQL/제약조건.sql
+++ 문서/SQL/제약조건.sql
@@ -17,7 +17,15 @@ unique : 유일한 값 보장
 check : 설정된 조건에 맞는 값만 저장
 default : 값을 안넣으면 자동으로 기본값을 저장
 foreign key : 외래키, FK
- */
+auto_increment : 숫자형 값의 자동증가
+*/
+
+/* primary key와 unique의 차이점
+PK : 테이블의 각 행을 고유하게 식별하는 컬럼. 테이블당 오직 한개만 가능.
+     자동으로 unique와 not null 속성을 가짐. 인덱스를 자동 생성
+unique : 테이블에서 고유한 값 보장. null 속성 가능.
+         인덱스를 자동 생성X, 테이블내에서 여러 개의 unique 컬럼을 가질 수 있음. 
+*/
 
 create table 학과
 (학과번호 char(2) primary key,
@@ -39,6 +47,34 @@ insert into 학생(학번, 이름, 생일, 연락처, 성별)
 values('S0001', 'Steve', '2000-01-01', '010-2222-3333', '남');
 insert into 학생(학번, 이름, 생일, 연락처, 성별)
 values('S0002', 'Tom', '2000-01-01', '010-4444-5555', '남');
+
+create table 과목
+(과목번호 char(5) primary key,
+과목명 varchar(20) not null,
+학점 int not null check(학점 between 2 and 4),
+구분 varchar(20) not null check(구분 in('전공','교양','일반')));
+
+create table 영화
+(영화번호 char(5) primary key,
+타이틀 varchar(100) not null,
+장르 varchar(20) check(장르 in ('코미디','드라마','다큐','SF','액션','기타')),
+배우 varchar(100) not null,
+감독 varchar(100) not null,
+개봉일 date,
+등록일 date default(curdate()));
+
+create table 평점
+(번호 int auto_increment primary key,
+평가자닉네임 varchar(50) not null,
+영화번호 char(5) not null references 영화(영화번호),
+평점 int not null check(평점 between 1 and 5),
+평가 varchar(2000) not null,
+등록일 date default(curdate()));
+
+
+
+
+
 
 
 
