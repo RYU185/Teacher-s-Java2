10c230007435d441be6d5293e4198d15ba99bb93
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2024-12-09 16:03:21 +0900

    Update MysqlJdbcExample_2.java

--- JDBC/src/MysqlJdbcExample_2.java
+++ JDBC/src/MysqlJdbcExample_2.java
@@ -1,2 +1,33 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class MysqlJdbcExample_2 {
+    // #1. 데이터베이스 커넥션 정보
+    private static final String URL = "jdbc:mysql://localhost:3306/testdb";
+    private static final String USER = "root";
+    private static final String PASSWORD = "root";
+
+    // #2. INSERT
+    // 부서테이블에 새로운 행 추가
+    public void insertDepartment(String deptNo, String deptName) {
+        String query = "insert into 부서(부서번호,부서명) "
+                + "values (?, ?)"; // ?자리에 매개변수를 넣을 예정
+        try(Connection conn = DriverManager.getConnection(URL,USER,PASSWORD);
+            PreparedStatement pstmt = conn.prepareStatement(query)) {
+                pstmt.setString(1, deptNo);
+                pstmt.setString(2, deptName);
+                pstmt.executeUpdate();
+            System.out.println("INSERT 성공");
+        }catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        MysqlJdbcExample_2 repository = new MysqlJdbcExample_2();
+        repository.insertDepartment("A6", "총무부");
+        repository.insertDepartment("A7", "해외영업부");
+    }
 }
