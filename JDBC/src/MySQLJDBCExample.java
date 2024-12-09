3969db2d6224569a43a55a09ada8b7165d0f03ed
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2024-12-09 11:07:30 +0900

    Update MySQLJDBCExample.java

--- JDBC/src/MySQLJDBCExample.java
+++ JDBC/src/MySQLJDBCExample.java
@@ -23,6 +23,24 @@ public class MysqlJdbcExample {
             Statement statement = connection.createStatement();
             ResultSet resultSet = statement.executeQuery(query)) {
             System.out.println("데이터베이스 연결 성공");
+            // 조회 결과가 resultSet에 담겨있음
+            // resultSet으로부터 데이터를 꺼내서 Customer클래스의 인스턴스에 저장
+            // 생성된 인스턴스들은 List<Customer>에 추가해야 함
+            while (resultSet.next()) {
+                Customer customer = new Customer();
+
+                customer.setCustomerId(resultSet.getString("고객번호"));
+                customer.setCompanyName(resultSet.getString("고객회사명"));
+                customer.setContactName(resultSet.getString("담당자명"));
+                customer.setContactTitle(resultSet.getString("담당자직위"));
+                customer.setAddress(resultSet.getString("주소"));
+                customer.setCity(resultSet.getString("도시"));
+                customer.setRegion(resultSet.getString("지역"));
+                customer.setPhone(resultSet.getString("전화번호"));
+                customer.setMileage(resultSet.getInt("마일리지"));
+
+                customers.add(customer); // 리스트에 추가
+            }
         } catch (SQLException e) {
             e.printStackTrace(); // 예외가 발생한 과정의 정보를 출력
         }
