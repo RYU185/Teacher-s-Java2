9afed90ec77006e349efe40ff9e7957e14e0b0c8
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2024-12-18 15:23:14 +0900

    Update CustomException_3.java

--- Exception/src/custom/CustomException_3.java
+++ Exception/src/custom/CustomException_3.java
@@ -17,5 +17,36 @@ class OverException extends RuntimeException {
     }
 }
 
-public class CustomException_3 {
+class AA {
+    // 여러개의 발생 가능한 예외를 함께 던질 수 있음
+    void checkScore(int score) throws MinusException, OverException {
+        if (score < 0) {
+            throw new MinusException("예외발생: 음수값입력");
+        }else if (score > 100) {
+            throw new OverException("예외발생: 100점 초과");
+        }else {
+            System.out.println("정상적인 값 : " + score);
+        }
+    }
 }
+
+public class CustomException_3 {
+    public static void main(String[] args) {
+        AA a = new AA();
+        try {
+            a.checkScore(85);
+            a.checkScore(150);
+            a.checkScore(-1);
+        }catch(MinusException | OverException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
+
+
+
+
+
+
+
+
