f794c575a9898abe56fcab8a62eceb309b57b391
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2024-12-18 15:15:27 +0900

    Create CustomException_3.java

---
+++ Exception/src/custom/CustomException_3.java
@@ -0,0 +1,21 @@
+package custom;
+
+class MinusException extends RuntimeException {
+    public MinusException() {
+        super();
+    }
+    public MinusException(String message) {
+        super(message);
+    }
+}
+class OverException extends RuntimeException {
+    public OverException() {
+        super();
+    }
+    public OverException(String message) {
+        super(message);
+    }
+}
+
+public class CustomException_3 {
+}
