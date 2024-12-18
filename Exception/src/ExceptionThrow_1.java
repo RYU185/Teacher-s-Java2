bad7b1c42aef21123adc7e479adf54931f6d8af6
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2024-12-18 12:40:53 +0900

    Update ExceptionThrow_1.java

--- Exception/src/ExceptionThrow_1.java
+++ Exception/src/ExceptionThrow_1.java
@@ -17,18 +17,20 @@ class B {
     void abc() {
         try{
             bcd();
+            System.out.println("예외 발생후 출력이 될까?");
         }catch (ArithmeticException e) {
             System.out.println("클래스 B 예외 발생");
             System.out.println(e.getMessage());
         }
     }
     void bcd() throws ArithmeticException {
-        System.out.println(3/0);
+        System.out.println(3/0); // 예외발생하나 예외전가처리
     }
 }
 
 public class ExceptionThrow_1 {
     public static void main(String[] args) {
-
+        B b = new B();
+        b.abc();
     }
 }
